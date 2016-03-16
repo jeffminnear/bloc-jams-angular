bd8d042c18703bddb49a4d772c8ed67754b0b513
Jeff Minnear <jeffminnear@gmail.com>
2016-03-16 14:04:58 -0400

    Refactor Collection view to use Fixtures service

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,16 @@
             return albumPicasso;
         };
 
+        Fixtures.getCollection = function getCollection(numberOfAlbums) {
+            var albums = [];
+
+            for (var i = 0; i < numberOfAlbums; i++) {
+                albums.push(albumPicasso);
+            }
+
+            return albums;
+        };
+
         return Fixtures;
     }
 
